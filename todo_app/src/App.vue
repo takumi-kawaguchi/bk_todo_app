395c5af5def969d6945f42a4457fdeec1f29c83e
Takumi Kawaguchi <tcgds4@gmail.com>
2020-11-15 23:52:09 +0900

    プロジェクト動作するように修正

--- todo_app/src/App.vue
+++ todo_app/src/App.vue
@@ -1,30 +1,23 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <h1>MyTodoApp</h1>
+    <div>
+      <label for="title">タイトル：</label>
+      <input type="text" v-model="title" placeholder="タイトル" />
+    </div>
+    <div>
+      <label for="body">内容：</label>
+      <textarea v-model="body" placeholder="内容"></textarea>
+    </div>
+    <pre>{{ $data }}</pre>
   </div>
-  <router-view/>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+export default {
+  data: () => ({
+    title: "",
+    body: "",
+  }),
+};
+</script>

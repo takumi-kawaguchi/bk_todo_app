3f54220f020b08f93b12546429d88484fe193448
Takumi Kawaguchi <tcgds4@gmail.com>
2020-11-16 23:06:11 +0900

    todoを追加・削除できるようにした

--- todo_app/src/App.vue
+++ todo_app/src/App.vue
@@ -9,6 +9,15 @@
       <label for="body">内容：</label>
       <textarea v-model="body" placeholder="内容"></textarea>
     </div>
+    <div>
+      <input type="submit" value="追加" @click="addList" />
+    </div>
+    <ul v-for="(item, i) in lists" :key="i">
+      <li>
+        id: {{ i }}, title: {{ item.title }}
+        <button @click="deleteList(i)">削除</button>
+      </li>
+    </ul>
     <pre>{{ $data }}</pre>
   </div>
 </template>
@@ -16,8 +25,20 @@
 <script>
 export default {
   data: () => ({
+    lists: [{ title: "mytodo", body: "content" }],
     title: "",
     body: "",
   }),
+  methods: {
+    addList: function () {
+      if (this.title == "" || this.body === "") return;
+      this.lists.push({ title: this.title, body: this.body });
+      this.title = "";
+      this.body = "";
+    },
+    deleteList: function (i) {
+      this.lists.splice(i, 1);
+    },
+  },
 };
 </script>

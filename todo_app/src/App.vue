0e24b8dd209a0fe98c05754e27f14d00d4511756
Takumi Kawaguchi <tcgds4@gmail.com>
2020-11-26 09:31:13 +0900

    vuedraggable導入、pkg管理をyarnに変更

--- todo_app/src/App.vue
+++ todo_app/src/App.vue
@@ -1,69 +1,51 @@
 <template>
   <div id="app">
-    <!-- <h1>MyTodoApp</h1>
-    <div>
-      <label for="title">タイトル：</label>
-      <input type="text" v-model="title" placeholder="タイトル" />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
-    <div>
-      <label for="body">内容：</label>
-      <textarea v-model="body" placeholder="内容"></textarea>
-    </div>
-    <div>
-      <input type="submit" value="追加" @click="addList" />
-    </div>
-    <draggable>
-      <ul v-for="(item, i) in lists" :key="i">
-        <li>
-          id: {{ i }}, title: {{ item.title }}
-          <button @click="deleteList(i)">削除</button>
-        </li>
-      </ul>
+    <p>テスト</p>
+    <draggable element="ul">
+      <li v-for="task in tasks" :key="task">{{ task }}</li>
     </draggable>
-    <pre>{{ $data }}</pre> -->
-    <draggable tag="ul">
-      <li v-for="item in items" :key="item.no">{{ item.name }}</li>
-    </draggable>
+    <!-- <router-view/> -->
   </div>
 </template>
 
 <script>
 import draggable from "vuedraggable";
 export default {
+  name: "app",
   components: {
-    draggable: draggable,
+    draggable,
   },
   data() {
     return {
-      items: [
-        { no: 1, name: "キャベツ", categoryNo: "1" },
-        { no: 2, name: "ステーキ", categoryNo: "2" },
-        { no: 3, name: "リンゴ", categoryNo: "3" },
-      ],
+      tasks: ["task1", "task2", "task3", "task4"],
     };
   },
 };
-// export default {
-//   name: "Draggable",
-//   components: { draggable },
-//   data: () => ({
-//     lists: [{ title: "mytodo", body: "content" }],
-//     title: "",
-//     body: "",
-//     options: {
-//       animation: 200,
-//     },
-//   }),
-//   methods: {
-//     addList: function () {
-//       if (this.title == "" || this.body === "") return;
-//       this.lists.push({ title: this.title, body: this.body });
-//       this.title = "";
-//       this.body = "";
-//     },
-//     deleteList: function (i) {
-//       this.lists.splice(i, 1);
-//     },
-//   },
-// };
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
